6aa5b1dac50cb3b9ae8a6f4e7382cd63691c63fd
leozak <leozak@gmail.com>
2026-06-02 10:05:47 -0300

    feat: teste de integração do usecase create-todo

--- src/core/todo/usecases/create-todo.usecase.test.ts
+++ src/core/todo/usecases/create-todo.usecase.test.ts
@@ -1,3 +1,49 @@
+import { makeTestTodoRepository } from "@/core/__tests__/utils/make-test-todo-repository";
+import { createTodoUseCase } from "./create-todo.usecase";
+import { InvalidTodo, ValidTodo } from "../schemas/todo.contract";
+
 describe("createTodoUseCase (integration)", () => {
-  test("", async () => {});
+  beforeEach(async () => {
+    const { deleteTodoNoWhere } = await makeTestTodoRepository();
+    await deleteTodoNoWhere();
+  });
+
+  afterAll(async () => {
+    const { deleteTodoNoWhere } = await makeTestTodoRepository();
+    await deleteTodoNoWhere();
+  });
+
+  test("deve retornar erro se a validação falhar", async () => {
+    const result = (await createTodoUseCase("")) as InvalidTodo;
+
+    expect(result.success).toBe(false);
+    expect(result.errors).toHaveLength(1);
+  });
+
+  test("deve retornar o TODO se a validação passar", async () => {
+    const description = "Isso deve funcionar";
+
+    const result = (await createTodoUseCase(description)) as ValidTodo;
+
+    expect(result.success).toBe(true);
+    expect(result.todo).toStrictEqual({
+      id: expect.any(String),
+      description,
+      createdAt: expect.any(String),
+    });
+  });
+
+  test("deve retornar erro se o repositório falhar", async () => {
+    const description = "Isso só funciona uma vez";
+
+    // Cria um todo
+    await createTodoUseCase(description);
+    // Tenta criar outro todo com a mesma descrição, o que deve falhar
+    const result = (await createTodoUseCase(description)) as InvalidTodo;
+
+    expect(result.success).toBe(false);
+    expect(result.errors).toStrictEqual([
+      "Já existe um todo com a mesma descrição ou id.",
+    ]);
+  });
 });
