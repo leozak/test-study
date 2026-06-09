7e7f8b0d1d3bba66ad9b04e0fab7d90c72f99126
leozak <leozak@gmail.com>
2026-06-09 14:30:37 -0300

    feat: testes do botão

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,5 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import { Button } from ".";
+import userEvent from "@testing-library/user-event";
+
+const VARIANT_DEFAULT_CLASSES = "bg-blue-600 hover:bg-blue-700 text-blue-100";
+const VARIANT_GHOST_CLASSES = "bg-slate-300 hover:bg-slate-400 text-slate-950";
+const VARIANT_DANGER_CLASSES = "bg-red-600 hover:bg-red-700 text-red-100";
+
+const SIZE_MD_CLASSES =
+  "text-base/tight py-2 px-4 rounded-md [&_svg]:w-4 [&_svg]:h-4 gap-2";
+const SIZE_SM_CLASSES =
+  "text-xs/tight py-1 px-2 rounded-sm [&_svg]:w-3 [&_svg]:h-3 gap-1";
+const SIZE_LG_CLASSES =
+  "text-lg/tight py-4 px-6 rounded-lg [&_svg]:w-5 [&_svg]:h-5 gap-3";
+
+const DISABLED_CLASSES =
+  "disabled:bg-slate-200 disabled:text-slate-400 disabled:cursor-not-allowed";
 
 describe("<Button />", () => {
   describe("props padrão e JSX", () => {
@@ -8,28 +23,108 @@ describe("<Button />", () => {
 
       const button = screen.getByRole("button", { name: /enviar formulário/i });
 
-      expect(button).toHaveClass("bg-blue-600 hover:bg-blue-700 text-blue-100");
-      expect(button).toHaveClass(
-        "text-base/tight py-2 px-4 rounded-md [&_svg]:w-4 [&_svg]:h-4 gap-2",
-      );
+      expect(button).toHaveClass(VARIANT_DEFAULT_CLASSES);
+      expect(button).toHaveClass(SIZE_MD_CLASSES);
     });
 
-    // test("verifica se as propriedades padrão do JSX funcionam corretamente", async () => {});
+    test("verifica se as propriedades padrão do JSX funcionam corretamente", async () => {
+      const handleClick = vi.fn();
+
+      render(
+        <Button onClick={handleClick} type="submit" aria-hidden="false">
+          Enviar formulário
+        </Button>,
+      );
+
+      const button = screen.getByText(/enviar formulário/i);
+
+      await userEvent.click(button);
+
+      expect(handleClick).toHaveBeenCalledTimes(1);
+      expect(button).toHaveAttribute("type", "submit");
+      expect(button).toHaveAttribute("aria-hidden", "false");
+    });
   });
 
-  // describe("variantes (cores)", () => {
-  // test("checa se default aplica a cor correta", async () => {});
-  // test("checa se ghost aplica a cor correta", async () => {});
-  // test("checa se danger aplica a cor correta", async () => {});
-  // });
+  describe("variantes (cores)", () => {
+    test("checa se default aplica a cor correta", async () => {
+      render(
+        <Button variant="default" title="botão">
+          Enviar formulário
+        </Button>,
+      );
 
-  // describe("variantes (tamanhos)", () => {
-  // test("tamanho sm deve ser menor", async () => {});
-  // test("tamanho md deve ser médio", async () => {});
-  // test("tamanho lg deve ser maior", async () => {});
-  // });
+      const button = screen.getByTitle(/botão/i);
 
-  // describe("disabled", () => {
-  // test("classes para estado desativado estão corretamente", async () => {});
-  // });
+      expect(button).toHaveClass(VARIANT_DEFAULT_CLASSES);
+    });
+    test("checa se ghost aplica a cor correta", async () => {
+      render(
+        <Button variant="ghost" title="botão">
+          Enviar formulário
+        </Button>,
+      );
+
+      const button = screen.getByTitle(/botão/i);
+
+      expect(button).toHaveClass(VARIANT_GHOST_CLASSES);
+    });
+    test("checa se danger aplica a cor correta", async () => {
+      render(
+        <Button variant="danger" title="botão">
+          Enviar formulário
+        </Button>,
+      );
+
+      const button = screen.getByTitle(/botão/i);
+
+      expect(button).toHaveClass(VARIANT_DANGER_CLASSES);
+    });
+  });
+
+  describe("variantes (tamanhos)", () => {
+    test("tamanho sm deve ser menor", async () => {
+      render(
+        <Button size="sm" title="botão">
+          Enviar formulário
+        </Button>,
+      );
+
+      const button = screen.getByTitle(/botão/i);
+
+      expect(button).toHaveClass(SIZE_SM_CLASSES);
+    });
+    test("tamanho md deve ser médio", async () => {
+      render(
+        <Button size="md" title="botão">
+          Enviar formulário
+        </Button>,
+      );
+
+      const button = screen.getByTitle(/botão/i);
+
+      expect(button).toHaveClass(SIZE_MD_CLASSES);
+    });
+    test("tamanho lg deve ser maior", async () => {
+      render(
+        <Button size="lg" title="botão">
+          Enviar formulário
+        </Button>,
+      );
+
+      const button = screen.getByTitle(/botão/i);
+
+      expect(button).toHaveClass(SIZE_LG_CLASSES);
+    });
+  });
+
+  describe("disabled", () => {
+    test("classes para estado desativado estão corretamente", async () => {
+      render(<Button disabled>Enviar formulário</Button>);
+
+      const button = screen.getByRole("button", { name: /enviar formulário/i });
+
+      expect(button).toHaveClass(DISABLED_CLASSES);
+    });
+  });
 });
